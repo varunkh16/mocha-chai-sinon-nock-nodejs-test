6e5ca55a8f0bd4ad6475150b8d463f2a12f6e73c
Varun KH <varun.kh16@gmail.com>
2024-02-03 15:03:43 +0530

    Basic test cases for calculator simple file

--- test/calculator.spec.ts
+++ test/calculator.spec.ts
@@ -11,5 +11,39 @@ describe('Test Calculator Class', () => {
         //assert
         expect(result).to.equal(6);
     });
+
+    it('should return difference', () => {
+        //arrange
+        const calc = new Calculator();
+        //act
+        const result = calc.subtract(2, 4);
+        //assert
+        expect(result).to.equal(-2);
+    });
+
+    it('should return product', () => {
+        //arrange
+        const calc = new Calculator();
+        //act
+        const result = calc.multiply(2, 4);
+        //assert
+        expect(result).to.equal(8);
+    });
+
+    it('should return quotient', () => {
+        //arrange
+        const calc = new Calculator();
+        //act
+        const result = calc.divide(2, 4);
+        //assert
+        expect(result).to.equal(0.5);
+    });
+
+    it('should throw error in quotient while divide by 0', () => {
+        //arrange
+        const calc = new Calculator();
+        //act and assert
+        expect(() => calc.divide(2, 0)).to.throw("Cannot multiply by zero");
+    });
     
 });
