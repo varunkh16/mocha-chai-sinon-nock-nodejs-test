cdec5f067f71310163f43294b2cb2a57cf1d7039
Varun KH <varun.kh16@gmail.com>
2024-02-04 15:46:27 +0530

    Added test suite for spy, stub and mock using Sinon

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,9 +1,11 @@
 class Calculator {
     add(a: number, b: number): number {
-        return a + b;
+        this.logMessage("adding two numbers");
+        return a + b + this.getRandomValue();
     }
 
     subtract(a: number, b: number): number {
+        this.logMessage("subtracting two numbers");
         return a - b;
     }
 
@@ -14,6 +16,14 @@ class Calculator {
     divide(a: number, b: number): number {
         if (b === 0) throw new Error("Cannot multiply by zero");
         return a / b;
+    }
+
+    getRandomValue() {
+        return Math.floor(Math.random() * 10 + 1);
+    }
+
+    logMessage(msg: string) {
+        console.log(msg);
     }
 }
 
--- test/spy-stub-mock.spec.ts
+++ test/spy-stub-mock.spec.ts
@@ -1,8 +1,12 @@
 import { expect } from "chai";
 import Calculator from "../src/calculator";
+import Sinon, { SinonMock, SinonSpy, SinonStub } from "sinon";
 
-describe('hooks test', () => {
+describe('spy stub mock test', () => {
     let calc: Calculator;
+    let spy: SinonSpy;
+    let stub: SinonStub;
+    let mock: SinonMock;
 
     before(() => {
         console.log("1. before");
@@ -11,17 +15,24 @@ describe('hooks test', () => {
 
     beforeEach(() => {
         console.log("2. before each");
+        mock = Sinon.mock(calc);
     });
 
     //nested test suite
     describe('add test suite', () => {
         it('should return sum', () => {
             //arrange
-            
+            spy = Sinon.spy(calc, 'add');
+            stub = Sinon.stub(calc, 'getRandomValue').returns(2);
+            let expectation = mock.expects('logMessage').exactly(1).withArgs("adding two numbers");
+
             //act
             const result = calc.add(2, 4);
+
             //assert
-            expect(result).to.equal(6);
+            expect(result).to.equal(8);
+            expect(spy.calledOnceWith(2, 4)).to.be.true;
+            expectation.verify();
         });
     });
 
@@ -29,18 +40,26 @@ describe('hooks test', () => {
     describe('subtract test suite', () => {
         it('should return difference', () => {
             //arrange
-    
+            spy = Sinon.spy(calc, 'subtract');
+            let expectation = mock.expects('logMessage').exactly(1).withArgs("subtracting two numbers");
+
             //act
             const result = calc.subtract(2, 4);
+            
             //assert
             expect(result).to.equal(-2);
+            expect(spy.calledOnceWith(2, 4)).to.be.true;
+            expectation.verify();
         });
     });
 
     afterEach(() => {
         console.log("3. after each");
+        if (spy) spy.restore();
+        if (stub) stub.restore();
+        if (mock) mock.restore();
     });
-    
+
     after(() => {
         console.log("4. after");
     });
