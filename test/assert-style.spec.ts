50d19a18dd29ad30f78c7f6c397242d1c507c1f3
Varun KH <varun.kh16@gmail.com>
2024-02-04 12:42:37 +0530

    Added should and assert style of assertions

--- test/assert-style.spec.ts
+++ test/assert-style.spec.ts
@@ -1,8 +1,8 @@
-import { expect } from "chai";
+import { expect, should, assert } from "chai";
 
 describe('Assert style test case', () => {
 
-    it('expect assert style', () => {
+    it('EXPECT assertion style', () => {
         //arrange
         const name: string = 'varun';
         const age: number = 33;
@@ -34,6 +34,75 @@ describe('Assert style test case', () => {
         expect(boolData).to.be.ok;
         expect(boolData).to.be.true;
         expect(boolData).to.be.a('boolean');
+    });
+
+    it('SHOULD assertion style', () => {
+        should();
+        //arrange
+        const name: string = 'varun';
+        const age: number = 33;
+        const person: Object = { name: 'varun', age: 33 };
+        const marks: Array<number> = [72, 45, 56, 82];
+        const boolData: boolean = true;
+
+        //act
+
+        //should
+        name.should.be.ok;
+        name.should.be.equal('varun');
+        name.should.be.not.equal('sharath');
+        name.should.be.a('string');
+
+        age.should.be.ok;
+        age.should.be.equal(33);
+        age.should.be.a('number');
+
+        person.should.be.ok;
+        person.should.be.a('object');
+        person.should.have.property('name').equal('varun');
+        person.should.have.property('age').equal(33);
+
+        marks.should.be.ok;
+        marks.should.be.a('array');
+        marks.should.have.length(4).that.include(45);
+
+        boolData.should.be.ok;
+        boolData.should.be.true;
+        boolData.should.be.a('boolean');
+    });
+
+    it('ASSERT assertion style', () => {
+        //arrange
+        const name: string = 'varun';
+        const age: number = 33;
+        const person: Object = { name: 'varun', age: 33 };
+        const marks: Array<number> = [72, 45, 56, 82];
+        const boolData: boolean = true;
+
+        //act
+
+        //should
+        assert.isOk(name);
+        assert.equal(name, 'varun');
+        assert.notEqual(name, 'sharath');
+        assert.typeOf(name, 'string');
+
+        assert.isOk(age);
+        assert.equal(age, 33);
+        assert.typeOf(age, 'number');
+
+        assert.isOk(person);
+        assert.typeOf(person, 'object');
+        assert.deepEqual(person, { name: 'varun', age: 33 });
+
+        assert.isOk(marks);
+        assert.typeOf(marks, 'array');
+        assert.deepEqual(marks, [72, 45, 56, 82]);
+        assert.include(marks, 45);
+
+        assert.isOk(boolData);
+        assert.isTrue(boolData);
+        assert.typeOf(boolData, 'boolean');
     });
 
 });
